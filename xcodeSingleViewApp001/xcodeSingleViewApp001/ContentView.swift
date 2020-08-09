a1075e2d3bfd19605a42e8e0697d29e872a1b195
Phillips Pham <philpham908@icloud.com>
2020-08-09 14:20:57 -0500

    add new Xcode single view app project to this repo: xcoderepo0001

---
+++ xcodeSingleViewApp001/xcodeSingleViewApp001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcodeSingleViewApp001
+//
+//  Created by Phil Pham on 8/9/20.
+//  Copyright © 2020 Phil Pham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Xcode 11!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
